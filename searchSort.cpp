0b8fb51cdc74c41861155a2625a6b0377ac91970
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-24 23:09:36 -0800

    lab8

--- searchSort.cpp
+++ searchSort.cpp
@@ -36,95 +36,21 @@ void SearchSort::startProgram() {
 
 
 /*********************************************************************
-** Description:     this functions gets the text files and adds the
-**                  data into the arrays
+** Description:     this functions calls a function that opens the
+**                  respective text files that are added to the data
+**                  arrays.
 *********************************************************************/
 void SearchSort::loadArrays() {
     openFile("original.txt", firstArray, 9);
     openFile("early.txt", firstArray, 10);
     openFile("middle.txt", firstArray, 10);
     openFile("end.txt", firstArray, 10);
-
-//    // get and copy first file data into array
-//    ifstream infile_original;
-//    infile_original.open("original.txt");
-//    if (infile_original.fail()) {
-//        cout << "WARNING! Cannot find file \"original.txt\"\n\n";
-//    }
-//
-//    int val;
-//    int index = 0;
-//    while (!infile_original.eof()) {
-//        infile_original >> val;
-//        firstArray[index] = val;
-//        index++;
-//    }
-//    for (int i = 0; i < 9; i++) {
-//        cout << firstArray[i] << " ";
-//    }
-//    cout << endl;
-//
-//    // reset index
-//    index = 0;
-//
-//    // get and copy second file data into array
-//    ifstream infile_early;
-//    infile_early.open("early.txt");
-//    if (infile_early.fail()) {
-//        cout << "WARNING! Cannot find file \"early.txt\"\n\n";
-//    }
-//    while (!infile_early.eof()) {
-//        infile_early >> val;
-//        secondArray[index] = val;
-//        index++;
-//    }
-//    for (int i = 0; i < 10; i++) {
-//        cout << secondArray[i] << " ";
-//    }
-//    cout << endl;
-//
-//    // reset index
-//    index = 0;
-//
-//    // get and copy third file data into array
-//    ifstream infile_middle;
-//    infile_middle.open("middle.txt");
-//    if (infile_middle.fail()) {
-//        cout << "WARNING! Cannot find file \"middle.txt\"\n\n";
-//    }
-//    while (!infile_middle.eof()) {
-//        infile_middle >> val;
-//        thirdArray[index] = val;
-//        index++;
-//    }
-//    for (int i = 0; i < 10; i++) {
-//        cout << thirdArray[i] << " ";
-//    }
-//    cout << endl;
-//
-//    // reset index
-//    index = 0;
-//
-//    // get and copy fourth file data into array
-//    ifstream infile_end;
-//    infile_end.open("end.txt");
-//    if (infile_end.fail()) {
-//        cout << "WARNING! Cannot find file \"end.txt\"\n\n";
-//    }
-//    while (!infile_end.eof()) {
-//        infile_end >> val;
-//        fourthArray[index] = val;
-//        index++;
-//    }
-//    for (int i = 0; i < 10; i++) {
-//        cout << fourthArray[i] << " ";
-//    }
-//    cout << endl;
 }
 
 /*********************************************************************
-** Description:     opens the file based on the parameters it receives
-**                  and copies data into the arrays
+** Description:     this functions receives parameters that specify
+**                  the name of the file to open, the array to add
+**                  the data to, and the size of the array
 *********************************************************************/
 void SearchSort::openFile(string fileName, int arr[], int size) {
     int val;
