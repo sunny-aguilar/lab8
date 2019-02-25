58c49dbc95f788cf2534fec0b07f97c00e471d90
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-24 21:33:03 -0800

    lab8

---
+++ menu.cpp
@@ -0,0 +1,9 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 26, 2019
+** Description:     Menu class holds the main menu and user prompts
+**                  for the program. The menu class lives inside of
+**                  the searchSort class and the menus are called
+**                  as needed from within the searchSort class.
+*********************************************************************/
+#include "menu.hpp"
---
+++ menu.hpp
@@ -0,0 +1,12 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            Feb 26, 2019
+** Description:     Menu class holds the main menu and user prompts
+**                  for the program. The menu class lives inside of
+**                  the searchSort class and the menus are called
+**                  as needed from within the searchSort class.
+*********************************************************************/
+#ifndef LAB8_MENU_HPP
+#define LAB8_MENU_HPP
+
+#endif //LAB8_MENU_HPP
