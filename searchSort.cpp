d38ef147893fc4175475c5e0b910d126cb0617f5
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-02 12:09:24 -0800

    lab8

--- searchSort.cpp
+++ searchSort.cpp
@@ -136,7 +136,8 @@ bool SearchSort::simpleSearch(int arr[], int size, int val) {
 /*********************************************************************
 ** Description:     this function calls another function for each
 **                  array to be sorted. The function called by this
- *                  function creates an output file for each
+ *                  function creates an output file for each array
+ *                  
 *********************************************************************/
 void SearchSort::sortOperations() {
     outputSortedFile(firstArray, 9);
